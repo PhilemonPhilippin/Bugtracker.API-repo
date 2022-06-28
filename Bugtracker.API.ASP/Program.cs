33e68cbf778cec8c43612665d9f360bb616e330b
Philemon Philippin <p_philippin@hotmail.com>
2022-06-28 23:42:04 +0200

    Post fonctionnel sur swagger

--- Bugtracker.API.ASP/Program.cs
+++ Bugtracker.API.ASP/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddSwaggerGen();
 // Config injection
 builder.Services.AddTransient<Connection>((service) =>
 {
-    return new Connection(builder.Configuration.GetConnectionString("CockxConnectionString"));
+    return new Connection(builder.Configuration.GetConnectionString("HerseConnectionString"));
 });
 
 // DAL

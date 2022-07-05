1eb1ffee37d81a48b5e86e975668388ce5d0d3e7
Tarcali0n <p_philippin@hotmail.com>
2022-07-05 08:41:58 +0200

    API functional i checked for it

--- Bugtracker.API.DB/Scripts/Seed.sql
+++ Bugtracker.API.DB/Scripts/Seed.sql
@@ -5,8 +5,9 @@ SET IDENTITY_INSERT [Member] ON;
 GO
 
 INSERT INTO [Member] ([Id_Member], [Pseudo], [Email], [Pswd_Hash], [Firstname], [Lastname])
- VALUES (1, 'Phil', 'test', 'philo@devteam.be', 'Philomène', 'Philipov'),
- (2, 'Corentintin', 'test2', 'corentin@gmail.com', 'Corentin', 'De Coning');
+ VALUES (1, 'Phil', 'philo@devteam.be', 'test12', 'Philomène', 'Philipov'),
+ (2, 'Corentintin', 'corentin@gmail.com', 'test34', 'Corentin', 'De Conninck'),
+ (3, 'Dom', 'dom@gmail.com', 'test56', 'Dominique', 'Lafitte');
 
 SET IDENTITY_INSERT [Member] OFF;
 GO

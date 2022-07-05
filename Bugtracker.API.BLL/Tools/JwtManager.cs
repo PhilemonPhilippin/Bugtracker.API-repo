376caf9c0b0e2621b1c3bc541a782ee790c1df4c
Tarcali0n <p_philippin@hotmail.com>
2022-07-05 16:00:25 +0200

    start of JWToken but need to push to work at home

---
+++ Bugtracker.API.BLL/Tools/JwtManager.cs
@@ -0,0 +1,49 @@
+﻿using Bugtracker.API.BLL.DataTransferObjects;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugtracker.API.BLL.Tools
+{
+    public class JwtManager
+    {
+        private string _issuer;
+        private string _audience; 
+        private string _secret;
+        public JwtManager(IConfiguration config)
+        {
+            _issuer = config.GetSection("JwtToken").GetSection("issuer").ToString();
+            _audience = config.GetSection("JwtToken").GetSection("audience").ToString();
+            _secret = config.GetSection("JwtToken").GetSection("secret").ToString();
+        }
+        public string GenerateToken(MemberDto memberDto)
+        {
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+            Claim[] myClaims = new Claim[]
+            {
+                new Claim(ClaimTypes.Sid, memberDto.IdMember.ToString()),
+                new Claim(ClaimTypes.Name, memberDto.Pseudo.ToString())
+            };
+
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(myClaims),
+                Issuer = _issuer,
+                Audience = _audience,
+                SigningCredentials = credentials
+            };
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            SecurityToken token = handler.CreateToken(descriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}

905a8b827cf388cc7171ec530d17275d49340abf
Tarcali0n <p_philippin@hotmail.com>
2022-07-12 11:58:10 +0200

    delete ticket done

--- Bugtracker.API.DB/StoredProcedures/TicketProcedures/PPSP_DeleteTicket.sql
+++ Bugtracker.API.DB/StoredProcedures/TicketProcedures/PPSP_DeleteTicket.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[PPSP_DeleteTicket]
-	@param1 int = 0,
-	@param2 int
+	@Id_Ticket INT
 AS
-	SELECT @param1, @param2
+	BEGIN
+		DELETE FROM [Ticket] WHERE [Id_Ticket] = @Id_Ticket;
+	END
 RETURN 0

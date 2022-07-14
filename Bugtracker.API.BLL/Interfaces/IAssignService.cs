ca3cd736959d1cd94b9009fbdbc21d72da4f5784
Philemon Philippin <p_philippin@hotmail.com>
2022-07-14 21:19:56 +0200

    did assign BLL

---
+++ Bugtracker.API.BLL/Interfaces/IAssignService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugtracker.API.BLL.Interfaces
+{
+    public interface IAssignService
+    {
+        int Add(int projectId, int memberId);
+        bool Remove(int assignId);
+    }
+}
---
+++ Bugtracker.API.BLL/Services/AssignService.cs
@@ -0,0 +1,29 @@
+﻿using Bugtracker.API.BLL.Interfaces;
+using Bugtracker.API.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugtracker.API.BLL.Services
+{
+    public class AssignService : IAssignService
+    {
+        private IAssignRepository _assignRepository;
+
+        public AssignService(IAssignRepository assignRepository)
+        {
+            _assignRepository = assignRepository;
+        }
+
+        public int Add(int projectId, int memberId)
+        {
+            return _assignRepository.Add(projectId, memberId);
+        }
+        public bool Remove(int assignId)
+        {
+            return _assignRepository.Remove(assignId);
+        }
+    }
+}

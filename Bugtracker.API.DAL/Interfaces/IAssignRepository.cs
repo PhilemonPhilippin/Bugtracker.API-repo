07cc2e744127c0bbf69c7f654a7f86caefda48af
Philemon Philippin <p_philippin@hotmail.com>
2022-07-14 16:54:07 +0200

    DAL for assigns done

---
+++ Bugtracker.API.DAL/Interfaces/IAssignRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugtracker.API.DAL.Interfaces
+{
+    public interface IAssignRepository
+    {
+        int Add(int projectId, int memberId);
+        bool Remove(int assignId);
+    }
+}
---
+++ Bugtracker.API.DAL/Repositories/AssignRepository.cs
@@ -0,0 +1,35 @@
+﻿using Bugtracker.API.ADO;
+using Bugtracker.API.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugtracker.API.DAL.Repositories
+{
+    public class AssignRepository : IAssignRepository
+    {
+        public Connection Connection { get; set; }
+
+        public AssignRepository(Connection connection)
+        {
+            Connection = connection;
+        }
+
+        public int Add(int projectId, int memberId)
+        {
+            Command cmd = new Command("PPSP_CreateAssign", true);
+            cmd.AddParameter("Assign_Time", DateTime.Now);
+            cmd.AddParameter("Project", projectId);
+            cmd.AddParameter("Member", memberId);
+            return (int)Connection.ExecuteScalar(cmd);
+        }
+        public bool Remove(int assignId)
+        {
+            Command cmd = new Command("PPSP_DeleteAssign", true);
+            cmd.AddParameter("Id_Assign", assignId);
+            return Connection.ExecuteNonQuery(cmd) == 1;
+        }
+    }
+}
